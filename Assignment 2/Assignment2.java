b89a5733ed79953b9f9be19f5b8b1b206977ba86
Shashwat Mishra <shashwat.mishra@zemosolabs.com>
2018-07-20 14:49:34 +0530

    Finished assignments 2 and 3 with documentation and in gedit

--- Assignment 2/Assignment2.java
+++ Assignment 2/Assignment2.java
@@ -47,7 +47,8 @@ public class Assignment2{
 }
 
 /**
- *Contains all methods needed to check for occurence of letters in a line. 
+ *Contains all methods needed to check for occurence of letters in a line. Makes the appropriate mappings
+ *and reduces mappings accordingly when a valid character is found. 
  *
  *@author Shashwat Mishra
 */
@@ -59,6 +60,15 @@ class Checkalphabets{
 /**
  *Makes a hashmap by extracting the characters from a string containing all uppercase and lowercase letters
  *and linking all letters to a default value of 0. 
+ *
+ *@param dictionary   A hashmap that is initially empty (with no links)
+ *@param letters      A String that contains all the uppercase and lowercase letters of the alphabet,used
+ *                    to generate the dictionary by fragmenting into chars
+ *
+ *
+ *
+ *@return dictionary  A hashmap that contains all the letters of the alphabet both uppercase and lowercase 
+ *                    initialized to a default value of 0
 */
 
 
@@ -67,10 +77,25 @@ class Checkalphabets{
 		
 		for (int letter_pos=0; letter_pos<letters.length(); letter_pos++){
 			dictionary.put(String.valueOf(letters.charAt(letter_pos)), 0);
-        }
+        	}
 			
 		return dictionary;
 	}
+
+
+/**
+ *Parses through the passed line input string and whenever a letter is found, reduces the corresponding 
+ *mapped value by 1 by calling the appropriate functions. Makes sure that a valid character of the alphabet
+ *is found before doing decrement
+ *
+ *@param line           The user entered sentence or string that needs to be checked for occurence of all the
+ *                      letters of the alphabet
+ *@param dictionary     Hashmap containing all the alphabets mapped to 0, to be reduced
+ *
+ *
+ *@return dictionary    Manipulated hashmap with all alphabets linked to negative values if found in line and 
+ *                      0 value if not found
+*/
 
 	public Map<String, Integer> checkstring(String line, Map<String, Integer> dictionary){
 
@@ -94,6 +119,22 @@ class Checkalphabets{
 		return dictionary;
 	}
 
+
+/**
+ *Used to reduce the linked mapping for the appropriate key. When the letter found in the entered line is of the lowercase
+ *this method is called and reduces the mapping for the letter found as well as the mapping for the uppercase of the letter
+ *found. Thus ensures that case insensitivity is maintained and a is considered the same as A.
+ *
+ *@param line          The string entered by the user to check for occurence of alphabets
+ *@param dictionary    mapping being manipulated according to the letter found
+ *@param loopcounter   the loopcounter used in the method that calls this method so as to retain the position of the letter at 
+ *                     which to reduce the mapping 
+ *                     
+ *
+ *@return dictionary   Manipulated hashmap with lowercase letters and corresponding uppercase letter mappings reduced
+*/
+
+
 	private Map<String, Integer> manipulateupper(String line, Map<String, Integer> dictionary, int loopcounter){
 
 		dictionary.put(String.valueOf(line.charAt(loopcounter)).toUpperCase(), dictionary.get(String.valueOf(line.charAt(loopcounter)).toUpperCase()) - 1);
@@ -103,7 +144,19 @@ class Checkalphabets{
 	}
 
 
-
+/**
+ *Used to reduce the linked mapping for the appropriate key. When the letter found in the entered line is of the uppercase
+ *this method is called and reduces the mapping for the letter found as well as the mapping for the lowercase of the letter
+ *found. Thus ensures that case insensitivity is maintained and a is considered the same as A.
+ *
+ *@param line          The string entered by the user to check for occurence of alphabets
+ *@param dictionary    mapping being manipulated according to the letter found
+ *@param loopcounter   the loopcounter used in the method that calls this method so as to retain the position of the letter at 
+ *                     which to reduce the mapping 
+ *                     
+ *
+ *@return dictionary   Manipulated hashmap with lowercase letters and corresponding uppercase letter mappings reduced
+*/
 
 	private Map<String, Integer> manipulatelower (String line, Map<String, Integer> dictionary, int loopcounter){
 
@@ -114,13 +167,22 @@ class Checkalphabets{
 	}
 
 		
-
-
+/**
+ *Checks the dictionary sequentially as to whether any key has been mapped to 0. Any letter mapped to 0 implies that it 
+ *did not occur in the line entered and hence kept its default value. If any letter didn't occur flag is set to 1 and returns
+ *to main
+ *
+ *@param dictionary   The manipulated hashmap that links each letter to the corresponding integer according to their occurence in 
+ *                    the string entered
+ *
+ *
+ *@return flag        Flag which checks whether all letters are present or not. If present returns 0 else returns 1
+*/
 
 	
 	
 
-	public int 	checkdictionary(Map<String, Integer> dictionary){
+	public int checkdictionary(Map<String, Integer> dictionary){
 		int flag=0;
 		for (Map.Entry<String,Integer> entry : dictionary.entrySet())
 			if(entry.getValue()==0){
---
+++ Assignment 3/Assignment3.java
@@ -0,0 +1,177 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.*;
+
+
+/**
+ *This program pings a host given by a URL or an IP address and calculates the median time taken
+ *to ping the host over a number of ping processes. The user is asked for the number of pings to 
+ *be performed and the ping command is run on the ping service provided by the system by creating a 
+ *process. From the data obtained using the ping, the time for the same at each instance is found 
+ *out using a regular expression. Once the time for each ping is obtained, the median value for the 
+ *collection of values is found by sorting the list in ascending order and finding the middle values
+
+@author Shashwat Mishra 
+*/
+public class Assignment3{
+
+
+	public static void main(String[] args){
+
+		Pingprocess URL1 = new Pingprocess();
+
+		URL1.makecommandlist();
+		URL1.pinging();
+		URL1.regex();
+		URL1.displaymedian();
+
+	}
+
+
+/**
+ *Asks the user for the number of probes of data to be sent as an integer input and then returns
+ *the value 
+ *
+ *@return pingnos   integer that indicates the number of probes to be performed 
+*/
+
+	public int getpings(){
+		Scanner in = new Scanner(System.in);
+                System.out.println("Enter the number of pings to be performed");
+		int pingnos = in.nextInt();
+		return pingnos;
+	}
+		
+}	
+
+
+
+/**
+ *Contains all methods for making a list of commands to be performed, using the list to 
+ *build a process and then use the output from the process to manipulate and extract 
+ *the time taken per probe and finally calculate the median of the probe times and print it
+
+@author Shashwat Mishra
+*/
+
+
+class Pingprocess{
+
+	Assignment3 A1 = new Assignment3();
+
+	private int number_of_pings = A1.getpings();
+	private String[] ping_results = new String[number_of_pings];
+	private ArrayList<String> commandList = new ArrayList<>();
+	private float[] times = new float[number_of_pings];
+
+	
+
+/**
+ *makes a command list that is used to build a ProcessBuilder process and start the ping process in 
+ *the machine service. Asks the user to enter the host name and appends ping to the name so as to 
+ *directly start the ping process
+*/
+
+	public void makecommandlist(){
+
+		commandList.add("ping");
+
+                Scanner in = new Scanner(System.in);
+                System.out.println("Enter IP address or URL of host");
+                commandList.add(in.next());
+
+	}
+
+
+/**
+ *Uses ProcessBuilder objects to start the command specified in the command list and ping the required host
+ *results are captured in a string array 
+ *
+ *@exception ex   prints the location and the exception that occured while accessing and starting the process
+*/
+
+	public void pinging(){
+		
+		try{
+
+		
+                	ProcessBuilder builder = new ProcessBuilder(commandList);
+                	Process pro = builder.start();
+
+                	BufferedReader input = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+             
+                	for (int loopvar=0; loopvar<number_of_pings;loopvar++)
+                	{
+                	    	ping_results[loopvar]=input.readLine();
+                	    	
+                	}
+		
+			
+		}
+
+		catch (Exception ex){
+                	ex.printStackTrace();
+            	}	
+	}
+
+
+/**
+ *Looks into the results obtained and stored by the ping process to locate and extract the time taken
+ *for the ping request. The time taken is located using a regular expression that matches the pattern of 
+ *results generally returned by a ping request. regular expression is compiled using a Pattern and Matcher 
+ *objects
+*/
+
+
+	public void regex(){
+
+                String theregex = "[0-9]+\\.+[0-9](?= ms) ";
+
+                Pattern checkRegex = Pattern.compile(theregex);
+
+		for(int loopvar=1; loopvar<number_of_pings; loopvar++) {
+
+                    	Matcher regexMatcher = checkRegex.matcher(ping_results[loopvar]);
+
+                    	while (regexMatcher.find()) {
+                        	if (regexMatcher.group().length() != 0) {
+                            		times[loopvar] = Float.valueOf(regexMatcher.group().trim());
+
+
+                        	}
+                    	}
+                } 
+
+	}        
+
+/**
+ *finds and prints the median of the obtained probe times. When the number of times obtained is odd, the median is 
+ *the middle value of the times. However when the number is even, the median is the average of the middle two times.
+ *prints the values formatted accordingly
+*/
+
+
+	public void displaymedian(){
+
+		Arrays.sort(times);
+
+                    if(number_of_pings%2==0){
+                        System.out.println("The median time to ping is "+times[(number_of_pings/2)]+ " ms");
+                    }
+                    else {
+                        System.out.println("The median time to ping is "+ (times[(number_of_pings+1)/2]+times[(number_of_pings-1)/2])/2+ " ms");
+                    }
+	}
+
+}
+
+
+	
+
+
+
+		
+		
